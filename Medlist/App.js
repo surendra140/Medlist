b2c377ac0d8cc6098996347c4c86bdf34d8b4c87
Surendra <surendrabankar218@gmail.com>
2022-04-18 13:50:48 +0530

    changes in app.js file

--- Medlist/App.js
+++ Medlist/App.js
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView
+    style={{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor:'#fff'
+    }}
+    >
+      <View>
+        <Text style={{fontSize:30, fontWeight:'bold', color:'#20315f'}}>Home</Text>
+      </View>
+      <TouchableOpacity>
+        <Text>
+          Let's Begin
+        </Text>
+        <MaterialIcons name='arrow-forward' size={22} color="#fff" />
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
